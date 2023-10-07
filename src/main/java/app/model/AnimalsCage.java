c6924f7803d6e98bab6fea0e04f4d46585a37554
Munira Turarova <muni.turaro@gmail.com>
2023-10-07 06:15:00 +0600

    '2.1.3'

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,30 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
-
+    private Animal cat;
+    private Animal dog;
+    private Timer time;
+    public AnimalsCage(@Qualifier("cat") Animal cat, @Qualifier("dog") Animal dog, Timer time){
+        this.cat = cat;
+        this.dog = dog;
+        this.time = time;
+    }
+    public Long getTimer(){
+        return time.getTime();
+    }
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(cat.toString() + "\t" + dog.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
 }
